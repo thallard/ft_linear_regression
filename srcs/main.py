a8a9647aef403f2c4100fd1aa6f98cdad8dac558
thallard <thallard@student.42lyon.fr>
2021-10-01 00:00:28 +0200

    Machine learning works

--- srcs/main.py
+++ srcs/main.py
@@ -31,43 +31,70 @@ def gradient_descent(X, y, theta, learning_rate, n_iterations):
     return theta, cost_history
 
 
+def normalize(x):
+    return (x - min(x)) / (max(x) - min(x))
+
+
+def normalize2(x, init):
+    return (x - min(init)) / (max(init) - min(init))
+
+
 # Using Pandas, read each information in .csv and create two NumPy arrays
 def parse_data():
-    df = pd.read_csv('../data.csv')
-    df.head()
-    df.astype('float64')
-    kms = df.to_numpy()
+    data = np.genfromtxt('../data.csv', delimiter=',')[1:]
+    x = data[:, 0]
+    y = data[:, 1]
 
-    temp_x, temp_y = kms.T
+    y = y.reshape(y.shape[0], 1)
+    x = x.reshape(x.shape[0], 1)
 
-    # Cast my temp Vector2 as float64
-    temp_x = temp_x.astype(np.float64)
-    temp_y = temp_y.astype(np.float64)
+    x = normalize(x)
 
-    temp_y = temp_y.reshape(temp_y.shape[0], 1)
-    temp_x = temp_x.reshape(temp_x.shape[0], 1)
-    x = temp_x / 10000
-    y = temp_y / 1000
     return x, y
+
+
+def coef_deter(y, predictions):
+    u = ((y - predictions) ** 2).sum()
+    v = ((y - y.mean()) ** 2).sum()
+    return 1 - u / v
 
 
 # Main function
 def main():
+    data = np.genfromtxt('../data.csv', delimiter=',')[1:]
     x, y = parse_data()
 
     X = np.hstack((x, np.ones(x.shape)))
 
     theta = np.random.randn(2, 1)
+    theta_random = np.random.randn(2, 1)
 
-    n_iterations = 5000
-    learning_rate = 0.01
-
-    theta_final, cost_history = gradient_descent(X, y, theta, learning_rate, n_iterations)
+    theta_final, cost_history = gradient_descent(X, y, theta, 0.1, 1000)
     predictions = model(X, theta_final)
+
+    print(theta_final)
+    print(theta[1] + theta[0] * normalize2(240000, data[:, 0]))
+
+    plt.subplot(2, 2, 1)
+    plt.title("Raw data")
+    plt.scatter(x, y)
+    plt.subplot(2, 2, 2)
+    plt.title("Before Machine Learning")
+    plt.scatter(x, y)
+    plt.plot(x, model(X, theta_random), c='r')
+    plt.tight_layout(pad=2.0)
+    plt.subplot(2, 2, 3)
+    plt.title("After Machine Learning")
     plt.scatter(x, y)
     plt.plot(x, predictions, c='r')
+    plt.subplot(2, 2, 4)
+    plt.title("Cost function")
+    plt.plot(range(1000), cost_history)
+    plt.tick_params()
     plt.show()
+    print(cost_history)
 
+    print(coef_deter(y, predictions))
 
 if __name__ == "__main__":
     main()
